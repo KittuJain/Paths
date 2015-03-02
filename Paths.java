6e8e8c57b0db9de9ba610c86ba411de8381182cf
Kittu Jain <cskratijain@gmail.com>
2015-03-02 16:40:31 +0530

    version 1 refactored for commandLine input

--- Paths.java
+++ Paths.java
@@ -1,23 +1,36 @@
 import java.util.Map;
 import java.util.Set;
-import java.util.List;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Paths {
 	static Map<String, List<String>> map = new HashMap<String, List<String>>();
 	
-	public void insertPaths (String source, List destination){
-		map.put(source, destination);
+	static{
+		List<String> bangalore = new ArrayList<String>();
+		List<String> singapore = new ArrayList<String>();
+		List<String> seoul = new ArrayList<String>();
+		List<String> beijing = new ArrayList<String>();
+		bangalore.add("Singapore");
+		singapore.add("Seoul");
+		singapore.add("Dubai");
+		seoul.add("Beijing");
+		beijing.add("Tokyo");
+		map.put("Bangalore", bangalore);
+		map.put("Singapore", singapore);
+		map.put("Seoul", seoul);
+		map.put("Beijing", beijing);
 	}
 
-	public static boolean isCityPresent(String city){
+	public boolean isCityPresent(String city){
 		Set<String> sourceStations = map.keySet(); //[Beijing, Singapore, Seoul, Bangalore]
 
 		if(sourceStations.contains(city))
 			return true;
 		else{
 			for(String source : sourceStations){
-				List<String> destinations = map.get(source ) ;
+				List<String> destinations = map.get(source) ;
 				if(destinations.contains(city))
 					return true;
 			}
@@ -27,5 +40,19 @@ public class Paths {
 
 	public boolean findFlight(String station1, String station2){
 		return map.get(station1).contains(station2);
+	}
+	public static void main(String[] args) {
+		String source = args[0];
+		String destination = args[1];
+		Paths paths = new Paths();
+		if(!paths.isCityPresent(source)){
+			System.out.println("City "+source+" doesn't have any flight");
+			return;
+		}
+		if(!paths.isCityPresent(destination)){
+			System.out.println("City "+destination+" doesn't have any flight");
+			return;
+		}else
+		System.out.println(paths.findFlight(source,destination));
 	}
 }
--- PathsTest.java
+++ PathsTest.java
@@ -1,31 +1,12 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.util.List;
-import java.util.ArrayList;
+
 
 public class PathsTest {
 	
-	private Paths paths() {
-		Paths path = new Paths();
-		List<String> bangalore = new ArrayList<String>();
-		List<String> singapore = new ArrayList<String>();
-		List<String> seoul = new ArrayList<String>();
-		List<String> beijing = new ArrayList<String>();
-		bangalore.add("Singapore");
-		singapore.add("Seoul");
-		singapore.add("Dubai");
-		seoul.add("Beijing");
-		beijing.add("Tokyo");
-		path.insertPaths("Bangalore", bangalore);
-		path.insertPaths("Singapore", singapore);
-		path.insertPaths("Seoul", seoul);
-		path.insertPaths("Beijing", beijing);
-		return path;
-	}
-
 	@Test
 	public void isCityPresent_checks_whether_city_is_prsent (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertFalse(paths.isCityPresent("Chennai"));
 		assertTrue(paths.isCityPresent("Dubai"));
 		assertTrue(paths.isCityPresent("Tokyo"));
@@ -34,37 +15,37 @@ public class PathsTest {
 
 	@Test
 	public void findFlight_returns_true_if_there_is_direct_flight_between_Bangalore_and_Singapore (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertTrue(paths.findFlight("Bangalore", "Singapore"));
 	}
 
 	@Test
 	public void findFlight_returns_false_if_there_is_no_flight_between_Bangalore_and_Tokyo (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertFalse(paths.findFlight("Bangalore", "Tokyo"));
 	}
 
 	@Test
 	public void findFlight_returns_true_if_there_is_direct_flight_between_Seoul_and_Beijing (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertTrue(paths.findFlight("Seoul", "Beijing"));
 	}
 
 	@Test
 	public void findFlight_returns_true_if_there_is_direct_flight_between_Beijing_and_Tokyo (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertTrue(paths.findFlight("Beijing", "Tokyo"));
 	}
 	
 	@Test
 	public void findFlight_returns_true_if_there_is_direct_flight_between_Singapore_and_Dubai (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertTrue(paths.findFlight("Singapore", "Dubai"));
 	}
 
 	@Test
 	public void findFlight_returns_true_if_there_is_direct_flight_between_Singapore_and_Seoul (){
-		Paths paths = paths();
+		Paths paths = new Paths();
 		assertTrue(paths.findFlight("Singapore", "Seoul"));
 	}
 	
