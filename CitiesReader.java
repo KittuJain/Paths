80a65d342cf2f2ea562b6b460d0f5896afe6c04d
Kittu Jain <cskratijain@gmail.com>
2015-03-06 17:25:43 +0530

    citiesReader file made

---
+++ CitiesReader.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+public class CitiesReader{
+	Map<String,String> countryRoutes = new HashMap<String,String>();
+	public void readCity() throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader("cities.txt"));
+		String line = ""+br.readLine();
+		while(line != null){
+			String places[] = line.split(",");
+			countryRoutes.put(places[0],places[1]);
+			line = br.readLine();
+		}
+		Set<String> cities = countryRoutes.keySet();
+		for (String city : cities) {
+			System.out.println(city+"["+getCountry(city)+"]");
+		}
+	}
+	public String getCountry(String city){
+		return countryRoutes.get(city);
+	}
+	public static void main(String[] args) throws IOException{
+		CitiesReader cr = new CitiesReader();
+		cr.readCity();
+	}
+}
